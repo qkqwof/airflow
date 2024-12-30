01a3dae3569c8db7cd4a4ad4106aa7077f2713db
qkqwof <qkqwof@yonsei.ac.kr>
2024-12-30 22:00:49 +0900

    진짜 마지막 수정

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -11,7 +11,7 @@ with DAG(
 ) as dag:
     bash_t1 = BashOperator(
         task_id = "bash_t1",
-        bash_command = 'echo "data_interval_end: {{ data_interal_end }}"'
+        bash_command = 'echo "data_interval_end: {{ data_interval_end }}"'
     )
 
     bash_t2 = BashOperator(
